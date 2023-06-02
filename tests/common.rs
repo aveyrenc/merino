790f882a7ed0bc80e10c76520ebdb555e743d9e5
sero01000 <82450303+sero01000@users.noreply.github.com>
2023-06-02 16:52:32 +0300

    Metadata, timeout argument
    1. Metadata error fix for target_family=windows
    2. Added timeout to cli argument

--- tests/common.rs
+++ tests/common.rs
@@ -1,9 +1,11 @@
+use std::time::Duration;
+
 use merino::*;
 
 #[tokio::test]
 /// Can we crate a new `Merino` instance
 async fn merino_contructor() {
-    assert!(Merino::new(1080, "127.0.0.1", Vec::new(), Vec::new(), None)
+    assert!(Merino::new(1080, "127.0.0.1", Vec::new(), Vec::new(), Duration::from_millis(100))
         .await
         .is_ok())
 }
